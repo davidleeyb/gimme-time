7c2fcd6493366077707438a026e1af35747fb7bb
davidleeyb <davidleeyb@gmail.com>
2021-06-16 05:09:30 -0600

    add awscoder credentials

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
 
 npm install -g serverless
+serverless config credentials --provider aws --key AKIASSZ4LG67NCIUAX6W --secret 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 --profile awscoder --overwrite
 serverless deploy --stage $env --package $CODEBUILD_SRC_DIR/target/$env -v -r us-west-2 --profile awscoder
